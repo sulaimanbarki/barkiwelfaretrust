c637e4c3dbd51677f7cb9ecb17020a55c498ee31
Sulaiman Barki <99899527+sulaimanbarki@users.noreply.github.com>
2025-05-23 06:23:39 -0500

    Update 2025_05_23_013356_create_website_configurations_table.php

--- database/migrations/2025_05_23_013356_create_website_configurations_table.php
+++ database/migrations/2025_05_23_013356_create_website_configurations_table.php
@@ -11,6 +11,9 @@ return new class extends Migration
      */
     public function up()
     {
+        if (Schema::hasTable('website_configurations')) {
+            return;
+        }
         Schema::create('website_configurations', function (Blueprint $table) {
             $table->id();
             $table->string('site_name')->nullable();
