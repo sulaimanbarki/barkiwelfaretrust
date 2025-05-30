c78bcf0fa22127b1a329344e0b7de108094e7a3a
Sulaiman Barki <99899527+sulaimanbarki@users.noreply.github.com>
2025-05-30 05:48:38 -0500

    Update CommonController.php

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -18,33 +18,44 @@ class CommonController extends Controller
     {
         try {
             $tables = DB::select('SHOW TABLES');
-    
+
             $output = '';
             foreach ($tables as $table) {
                 $table = get_object_vars($table);
                 $tableName = current($table);
-    
+
+                // Get the CREATE TABLE statement
                 $showTableQuery = "SHOW CREATE TABLE $tableName";
                 $createTable = DB::select($showTableQuery)[0]->{'Create Table'};
                 $output .= "\n\n" . $createTable . ";\n\n";
-    
+
+                // Get all rows from the table
                 $selectRowsQuery = "SELECT * FROM $tableName";
                 $rows = DB::select($selectRowsQuery);
-    
+
                 foreach ($rows as $row) {
                     $row = (array)$row;
-                    $values = implode("', '", array_map('addslashes', $row));
-                    $output .= "INSERT INTO $tableName VALUES ('$values');\n";
+
+                    // Handle NULLs and escape strings
+                    $escapedValues = array_map(function ($value) {
+                        if (is_null($value)) {
+                            return 'NULL';
+                        }
+                        return "'" . addslashes($value) . "'";
+                    }, $row);
+
+                    $values = implode(', ', $escapedValues);
+                    $output .= "INSERT INTO $tableName VALUES ($values);\n";
                 }
             }
-    
+
             // Set file name and path
             $fileName = 'database_backup_' . date('Y-m-d_H-i-s') . '.sql';
             $filePath = storage_path('app/' . $fileName);
-    
+
             // Write output to the backup file
             file_put_contents($filePath, $output);
-    
+
             // Set headers for file download
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
@@ -54,10 +65,10 @@ class CommonController extends Controller
             header('Cache-Control: must-revalidate');
             header('Expires: 0');
             readfile($filePath);
-    
+
             // Delete the backup file after download
             unlink($filePath);
-    
+
             exit;
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
